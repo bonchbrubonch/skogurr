a65b63850645bebd6f8cd67d36198cd04070a5e7
Artem <nikolloteslla@gmail.com>
2024-03-31 20:37:37 +0300

    add animations for example

--- app/js/app.js
+++ app/js/app.js
@@ -167,3 +167,24 @@ window.addEventListener('scroll', function onScroll() {
     }, 5);
   }
 });
+
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    } else{
+      change.target.classList.remove('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
